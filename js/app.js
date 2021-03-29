5ed1cd8881d4904fcd1b63504f96ae0f1aecc456
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-03-29 18:39:52 +0200

    dividing the structure into target sections, adding a background of homePage with zooming on Scroll  effect

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+// after reloading, the page is always on the top
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
 
+(function ($) {
+  $window = $(window);
+  $htmlbody = $("html,body");
+  $slide = $(".homeSlide");
+  $body = $("body");
+
+  function adjustWindow() {
+    var s = skrollr.init();
+    // get window size
+    winH = $window.height();
+    if (winH <= 550) {
+      winH = 550;
+    }
+    $slide.height(winH * 2);
+    s.refresh($(".homeSlide"));
+  }
+
+  adjustWindow();
+})(jQuery);
