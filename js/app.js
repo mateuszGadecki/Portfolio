14c318aaa9e6c6397241cc1e303672f5e6e9641d
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-04-01 07:00:50 +0200

    Adding navigation to the homePage

--- js/app.js
+++ js/app.js
@@ -11,7 +11,18 @@ window.onbeforeunload = function () {
 
   function adjustWindow() {
     var s = skrollr.init();
-    // get window size
+    // Scroll to scene on button click
+    skrollr.menu.init(s, {
+      animate: true,
+      easing: "linear",
+      scale: 2,
+      duration: function (currentTop, targetTop) {
+        return 1500;
+      },
+      complexLinks: false,
+      change: function (newHash, newTopPosition) {},
+      updateUrl: false,
+    });
     winH = $window.height();
     if (winH <= 550) {
       winH = 550;
